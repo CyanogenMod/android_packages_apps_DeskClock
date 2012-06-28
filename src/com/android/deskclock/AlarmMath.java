5c626574a2850cc7ce25ccf829bb41cb7a431130
Daveee10 <daver16@msn.com>
2012-06-28 20:26:11 +0200

    Fix: Fix Math - Double "9" button

--- src/com/android/deskclock/AlarmMath.java
+++ src/com/android/deskclock/AlarmMath.java
@@ -127,7 +127,7 @@ public class AlarmMath extends Activity {
 				updateText();
 			}
 		});
-		button.setText("9");
+		button.setText("0");
 		button = (Button) findViewById(R.id.math_0);
 		button.setOnClickListener(new OnClickListener() {
 			@Override
